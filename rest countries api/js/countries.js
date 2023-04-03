969db58dec236c9e3020e0dd8eab10dc1ac96d48
soubj <sobujahmed124@gmail.com>
2023-04-03 20:28:11 +0600

    getting data from api

--- rest countries api/js/countries.js
+++ rest countries api/js/countries.js
@@ -5,7 +5,20 @@ const loadRestCountries = () => {
     .catch((err) => console.log("error: ", err.message));
 };
 
-const displayCountries = (data) => {
-  console.log(data);
+const displayCountries = (countries) => {
+  //console.log(countries);
+  const allCountriesHTML = countries.map((country) => getCountryHTML(country));
+  console.log(allCountriesHTML[0]);
+  const container = document.getElementById("countries");
+  container.innerHTML = allCountriesHTML;
 };
+
+const getCountryHTML = (country) => {
+  return `
+    <div>
+        <h2>${country.name.common}</h2>
+    </div>
+    `;
+};
+
 loadRestCountries();
